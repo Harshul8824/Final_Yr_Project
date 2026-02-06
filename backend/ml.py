ce46592e5d94c5b7ecebe64e2e548198651bbdbd
Harshul Goswami <goswamiharshul88@gmail.com>
2026-02-06 15:59:52 +0530

    add some dummy code

---
+++ backend/ml.py
@@ -0,0 +1,34 @@
+a = [8, 3, 5, 1, 9, 12]
+res = a[0]
+for val in a:
+    if val < res:
+        res = val
+
+print(res)
+
+a = [8, 3, 5, 1, 9, 12]
+res = a[0]
+for val in a:
+    if val < res:
+        res = val
+
+print(res)
+
+
+a = [8, 3, 5, 1, 9, 12]
+res = a[0]
+for val in a:
+    if val < res:
+        res = val
+
+print(res)
+
+a = [8, 3, 5, 1, 9, 12]
+res = a[0]
+for val in a:
+    if val < res:
+        res = val
+
+print(res)
+
+
